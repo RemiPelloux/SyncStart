9561d9809ddaec08f9b1af166c7f02a8c87cd82f
remipelloux <pellouxremi@gmail.com>
2023-12-31 12:53:53 +0100

    Add login redirection and passport saving

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,22 @@
 // src/components/HomePage.jsx
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext.jsx";
 
 function HomePage() {
+    const { isLoggedIn } = useAuth();
+
     return (
         <div>
             <h1>Welcome to SyncStart</h1>
             <h2>
-                <Link to="/login">Login</Link> or <Link to="/register">Register</Link> to get started!
+                {isLoggedIn ? (
+                    <p>You are logged in!</p>
+                ) : (
+                    <p>
+                        <Link to="/login">Login</Link> or <Link to="/signup">Signup</Link> to get started.
+                    </p>
+                )}
             </h2>
         </div>
     );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,45 +1,74 @@
 // src/components/Login.jsx
-import React, { useState } from "react";
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {useAuth} from "../context/AuthContext.jsx";
 
-function Login() {
+function Login({onLoginSuccess}) {
+    const navigate = useNavigate();
+    const {login} = useAuth();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
             const response = await fetch('http://localhost:8080/login', {
                 method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ email, password }),
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({email, password}),
             });
-            const data = await response.json();
-            // Handle response
-            console.log(data);
-        } catch (error) {
-            console.error("Login error", error);
+            if (response.ok) {
+                login();
+                navigate("/"); // Redirect to homepage after login
+            } else {
+                setError("Failed to login. Please check your credentials.");
+            }
+        } catch (err) {
+            setError("An error occurred. Please try again later.");
         }
     };
 
     return (
-        <div className="max-w-sm mx-auto p-6">
-            <h1 className="text-xl font-semibold mb-4">Login</h1>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="Email"
-                    required
-                />
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Password"
-                    required
-                />
-                <button type="submit">Login</button>
+        <div className="max-w-md mx-auto mt-10">
+            <h1 className="text-2xl font-bold mb-5 text-center">Login</h1>
+            <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                        Email
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="email"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Email"
+                        required
+                    />
+                </div>
+                <div className="mb-6">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                        Password
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        id="password"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Password"
+                        required
+                    />
+                </div>
+                {error && <p className="text-red-500 text-xs italic">{error}</p>}
+                <div className="flex items-center justify-between">
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit">
+                        Sign In
+                    </button>
+                </div>
             </form>
         </div>
     );
